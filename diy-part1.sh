53bdfe94aa4db0938ad6b940adc7809704dec206
IllyasvielEin <swindyasuo@outlook.com>
2026-01-09 00:47:49 +0800

    refactor: 统一配置，添加 MosDNS 替代 AdBlock
    - 精简 .config: 移除冗余包 (openlist, naiveproxy, xray, fuse, avahi 等)
    - 统一两个配置的 LuCI Apps (Passwall, TurboAcc, MosDNS, Argon 等)
    - 添加 MosDNS + v2dat 实现 DNS 分流和广告过滤
    - 移除 AdBlock (由 MosDNS 内置广告规则替代)
    - diy-part1.sh: 添加 MosDNS 和 v2ray-geodata 克隆
    - diy-part2.sh: 启用默认 IP (192.168.6.1) 和主机名 (Ruby6000)

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,25 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i '1i src-git danshui https://github.com/281677160/openwrt-package.git;immortalwrt' feeds.conf.default
-# sed -i '2i src-git dstheme https://github.com/281677160/openwrt-package.git;Theme2' feeds.conf.default
-#echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default   
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# add alist
-# rm -rf feeds/packages/net/alist
-# rm -rf feeds/luci/applications/luci-app-alist
-# git clone https://github.com/sbwml/luci-app-alist feeds/alist
-# mv ./feeds/alist/alist ./feeds/packages/net
-# mv ./feeds/alist/luci-app-alist feeds/luci/applications/
-# rm -rf feeds/alist
+# ===========================================
+# MosDNS + v2ray-geodata (DNS 分流 + 广告过滤)
+# ===========================================
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
--- diy-part2.sh
+++ diy-part2.sh
@@ -9,74 +9,23 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# 解决冲突（适用于kenzok8插件源码）
-# rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-# rm -rf feeds/packages/utils/v2dat
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# 设置主路由静态IP
-# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+# ===========================================
+# 1. 设置默认 IP 为 192.168.6.1
+# ===========================================
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 设置密码为空
+# ===========================================
+# 2. 设置默认主机名为 Ruby6000
+# ===========================================
+sed -i 's/ImmortalWrt/Ruby6000/g' package/base-files/files/bin/config_generate
+
+# ===========================================
+# 以下为注释掉的可选配置，按需启用
+# ===========================================
+
+# 设置密码为空（方便首次登录）
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-# 安装新主题 luci-theme-bootstrap-mod
-# git clone https://github.com/leshanydy2022/luci-theme-bootstrap-mod.git package/lean/luci-theme-bootstrap-mod
-# sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' feeds/luci/collections/luci/Makefile
-
-# 安装luci-app-smartdns和luci-app-adguardhome
-# rm -rf feeds/luci/applications/luci-app-smartdns
-#rm -rf feeds/luci/applications/luci-app-adguardhome
-# git clone https://github.com/leshanydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-#env GIT_TERMINAL_PROMPT=0 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-
-# 为adguardhome插件更换最新的版本
-# rm -rf feeds/packages/net/adguardhome
-# env GIT_TERMINAL_PROMPT=0 git clone https://github.com/kenzok8/adguardhome.git feeds/packages/net/adguardhome
-
-# 安装uugamebooster
-# rm -rf feeds/luci/applications/luci-app-uugamebooster
-# git clone https://github.com/datouha/luci-app-uugamebooster.git package/lean/luci-app-uugamebooster
-# rm -rf feeds/packages/net/uugamebooster
-# git clone https://github.com/datouha/uugamebooster.git feeds/packages/net/uugamebooster
-
-
-
-# ---------------------------------------------------------------
-## OpenClash
-# git clone -b v0.46.086 --depth=1 https://github.com/vernesong/openclash.git OpenClash
-# rm -rf feeds/luci/applications/luci-app-openclash
-# mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
-# ---------------------------------------------------------------
-
-# ##------------- meta core ---------------------------------
-# wget https://github.com/MetaCubeX/mihomo/releases/download/v1.19.9/mihomo-linux-arm64-v1.19.9.gz
-# gzip -d mihomo-linux-arm64-v1.19.9.gz
-# chmod +x mihomo-linux-arm64-v1.19.9 >/dev/null 2>&1
-# mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-# mv mihomo-linux-arm64-v1.19.9 feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
-# ##---------------------------------------------------------
-
-# ##-------------- GeoIP 数据库 -----------------------------
-# curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o /tmp/GeoIP.dat
-# mv /tmp/GeoIP.dat feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoIP.dat >/dev/null 2>&1
-# ##---------------------------------------------------------
-
-# ##-------------- GeoSite 数据库 ---------------------------
-# curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o /tmp/GeoSite.dat
-# mv -f /tmp/GeoSite.dat feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoSite.dat >/dev/null 2>&1
-
-# 为smartDNS插件更换最新的版本
-# rm -rf feeds/packages/net/smartdns
-# git clone https://github.com/leshanydy2022/smartdns.git feeds/packages/net/smartdns
-
-# Modify hostname
-# sed -i 's/ImmortalWrt/ImmortalWrt-YDY/g' package/base-files/files/bin/config_generate
-
-# Modify filename, add date prefix
-#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
-
-# Modify ppp-down, add sleep 3
-# sed -i '$a\\\nsleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
+# 修改固件文件名添加日期前缀
+# sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
